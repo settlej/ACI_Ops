cd90b8f06e46b096a0033b1778246f3ddaf60f6b
settlej <joshuabsettle@gmail.com>
2019-11-15 15:14:34 -0500

    modify node info

--- information/switchandapicinfo.py
+++ information/switchandapicinfo.py
@@ -47,35 +47,79 @@ f_handler.setFormatter(f_format)
 logger.addHandler(c_handler)
 logger.addHandler(f_handler)
 
+
+class customFabricNode():
+    def __init__(self,kwargs):
+        self.__dict__.update(**kwargs)
+    def __setattr__(self, key, value):
+        self.__dict__[key] = value
+    def __repr__(self):
+        return self.dn
+
+def requestnodeinfo(url, cookie, q):
+    result = GetResponseData(url, cookie)
+    q.put(result)
+    
+
 def main(import_apic,import_cookie):
     while True:
         global apic
         global cookie
         cookie = import_cookie
         apic = import_apic
-        url = """https://{apic}/api/node/class/topSystem.json""".format(apic=apic)
+        cnodelist = []
+        nodethreadlist = []
+        urllist = []
+        q = Queue.Queue()
+        url = """https://{apic}/api/class/fabricNode.json""".format(apic=apic)
         result = GetResponseData(url, cookie)
-        xlocation = 6
-        ylocation = 3
-        clear_screen()
-        for node in sorted(result, key=lambda a: int(a['topSystem']['attributes']['id']) ):
-            #if location == 4:
-            #    location = 1
-            print('*' * 133)
-            a = ('{}'.format('\x1b[' + str(ylocation) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['podId'] + '\x1b[0m'))
-            print(a)
-            print('{}'.format('\x1b[' + str(ylocation+1) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['id'] + '\x1b[0m'))
-            print('{}'.format('\x1b[' + str(ylocation+2) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['systemUpTime'] + '\x1b[0m'))
-            print('{}'.format('\x1b[' + str(ylocation+3) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['currentTime'] + '\x1b[0m'))
-            print('{}'.format('\x1b[' + str(ylocation+4) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['inbMgmtAddr'] + '\x1b[0m'))
-            print('{}'.format('\x1b[' + str(ylocation+5) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['oobMgmtAddr'] + '\x1b[0m'))
-            print('{}'.format('\x1b[' + str(ylocation+6) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['role'] + '\x1b[0m'))
-            print('{}'.format('\x1b[' + str(ylocation+7) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['serial'] + '\x1b[0m'))
-            print('{}'.format('\x1b[' + str(ylocation+8) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['version'] + '\x1b[0m'))
-            print('{}'.format('\x1b[' + str(ylocation+9) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['status'] + '\x1b[0m'))
-            xlocation += 33
-            if xlocation >= 133:
-                ylocation += 12
-                xlocation = 6
+        for node in sorted(result, key=lambda a: int(a['fabricNode']['attributes']['id']) ):
+            print(node['fabricNode']['attributes']['id'])
+            cnodelist.append(customFabricNode(node['fabricNode']['attributes']))
+       #url = """https://{apic}/api/node/class/topSystem.json""".format(apic=apic)
+        for u in cnodelist:
+            if u.fabricSt == 'active' or u.role == 'controller':
+                urllist.append("""https://{apic}/api/class/topSystem.json?query-target-filter=eq(topSystem.id,"{nodeid}")""".format(apic=apic,nodeid=u.id))
+        for url in urllist:
+            t = threading.Thread(target=requestnodeinfo, args=[url,cookie,q])
+            t.start()
+            nodethreadlist.append(t)
+        for x in nodethreadlist:
+            x.join()
+            nodeid = q.get()
+            print(nodeid)
+        
+
+
+     #   result = GetResponseData(url, cookie)
+     #   xlocation = 6
+     #   ylocation = 3
+     #   clear_screen()
+     #   cnodelist = []
+     #   for node in sorted(result, key=lambda a: int(a['topSystem']['attributes']['id']) ):
+     #       #if location == 4:
+     #       #    location = 1
+     #       #print(node['topSystem']['attributes'])
+     #       cnodelist.append(customFabricNode(node['topSystem']['attributes']))
+     #   for x in cnodelist:
+     #       print(x.dn)
+  #
+#
+     #      # print('*' * 133)
+     #      # a = ('{}'.format('\x1b[' + str(ylocation) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['podId'] + '\x1b[0m'))
+           # print(a)
+           # print('{}'.format('\x1b[' + str(ylocation+1) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['id'] + '\x1b[0m'))
+           # print('{}'.format('\x1b[' + str(ylocation+2) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['systemUpTime'] + '\x1b[0m'))
+           # print('{}'.format('\x1b[' + str(ylocation+3) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['currentTime'] + '\x1b[0m'))
+           # print('{}'.format('\x1b[' + str(ylocation+4) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['inbMgmtAddr'] + '\x1b[0m'))
+           # print('{}'.format('\x1b[' + str(ylocation+5) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['oobMgmtAddr'] + '\x1b[0m'))
+           # print('{}'.format('\x1b[' + str(ylocation+6) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['role'] + '\x1b[0m'))
+           # print('{}'.format('\x1b[' + str(ylocation+7) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['serial'] + '\x1b[0m'))
+           # print('{}'.format('\x1b[' + str(ylocation+8) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['version'] + '\x1b[0m'))
+           # print('{}'.format('\x1b[' + str(ylocation+9) + ';' + str(xlocation) + 'H' + node['topSystem']['attributes']['status'] + '\x1b[0m'))
+           # xlocation += 33
+           # if xlocation >= 133:
+           #     ylocation += 12
+           #     xlocation = 6
         break
     custom_raw_input('\nPress enter to continue...')
