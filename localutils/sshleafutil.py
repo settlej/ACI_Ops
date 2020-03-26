a6bc26dea84d4303025d7937b1805c2d97264ef5
settlej <joshuabsettle@gmail.com>
2020-03-25 22:59:34 -0400

    Added iping options

--- localutils/sshleafutil.py
+++ localutils/sshleafutil.py
@@ -18,17 +18,35 @@ def remotesshcommand(cmd,user,device):
     print('SSHing to leaf to perform iping: "{cmd}"'.format(user=user,device=device,cmd=cmd))
     os.system(sshsequence)
 
-def gather_vrfs(apic,cookie):
-    url = """https://{apic}/api/node/class/fvCtx.json?&order-by=fvCtx.modTs|desc""".format(apic=apic)
+class local_vrfobj():
+    def __init__(self, kwargs):
+        self.__dict__.update(**kwargs)
+    def __repr__(self):
+        return self.name
+
+def gather_vrfs(apic,cookie,leaf):
+    url = """https://{apic}/api/node/class/topology/pod-1/node-{leaf}/l3Ctx.json?&order-by=l3Ctx.modTs|desc""".format(apic=apic,leaf=leaf)
     results = GetResponseData(url,cookie=cookie)
-    dnlist = (x['fvCtx']['attributes']['dn'] for x in results)
+    vrfobjlist = [local_vrfobj(x['l3Ctx']['attributes']) for x in results]
+    #namelist = (x['l3Ctx']['attributes']['name'] for x in results)
     tenant_vrf_list = []
-    for vrf in dnlist:
-        tv = vrf.split('/')[1:]
-        tenant = tv[0].replace('tn-','')
-        vrf = tv[1].replace('ctx-','')
-        tenant_vrf_list.append((tenant, vrf))
+    for vrfobj in vrfobjlist:
+        scope = vrfobj.scope
+        if ':' in vrfobj.name:
+            tenant, vrf = vrfobj.name.split(':')
+            tenant_vrf_list.append((scope,[tenant, vrf]))
+        else:
+            tenant_vrf_list.append((scope,[vrf,""]))
+    #import pdb; pdb.set_trace()
     return tenant_vrf_list
+    #import pdb; pdb.set_trace()
+    #tenant_vrf_list = []
+    #for vrf in dnlist:
+    #    tv = vrf.split('/')[1:]
+    #    tenant = tv[0].replace('tn-','')
+    #    vrf = tv[1].replace('ctx-','')
+    #    tenant_vrf_list.append((tenant, vrf))
+    #return namelist
 
 def gather_l2BD(apic,cookie,leaf):
     url = """https://{apic}/api/node/class/topology/pod-1/node-{leaf}/l2BD.json""".format(apic=apic,leaf=leaf)
@@ -40,18 +58,20 @@ def gather_l2BD(apic,cookie,leaf):
     l2bdict = {l2bd.id:l2bd.__dict__ for l2bd in l2bdobjlist}
     return l2bdict
 
-def gather_svi(apic,cookie,leaf):
+def gather_svi(apic,cookie,scope,leaf):
     url = """https://{apic}/api/node/class/topology/pod-1/node-{leaf}/sviIf.json""".format(apic=apic,leaf=leaf)
     results = GetResponseData(url,cookie=cookie)
-    dnlist = (x['sviIf']['attributes']['dn'] for x in results)
-    svi = (x['sviIf']['attributes']['dn'] for x in results)
-    tenant_vrf_list = []
-    for vrf in dnlist:
-        tv = vrf.split(':')
-        tenant = tv[0]
-        vrf = tv[1]
-        tenant_vrf_list.append((tenant, vrf))
-    return tenant_vrf_list
+    svilist = []
+    for svi in results:
+        if scope in svi['sviIf']['attributes']['dn']:
+            svilist.append(svi['sviIf']['attributes']['id'])
+    #dnlist = (x['sviIf']['attributes']['dn'] for x in results)
+    #svi = [x['sviIf']['attributes']['id'] for x in results]
+    #svilist = []
+    #for vrf in dnlist:
+
+     #   tenant_vrf_list.append((tenant, vrf))
+    return svilist
 
 def main(import_apic,import_cookie,user):
     global apic
@@ -81,23 +101,27 @@ def main(import_apic,import_cookie,user):
         url = """https://{apic}/api/node/class/topSystem.json?query-target-filter=and(eq(topSystem.id,"{chosenleafs}"))&order-by=fabricNode.modTs|desc""".format(apic=apic,chosenleafs=chosenleafs[0])
         results = GetResponseData(url,cookie)
         deviceip = results[0]['topSystem']['attributes']['oobMgmtAddr']
-        gather_l2BD(apic,cookie,leaf=chosenleafs[0])
-        tenant_vrf_list = gather_vrfs(apic,cookie)
+        tenant_vrf_list = gather_vrfs(apic,cookie,leaf=chosenleafs[0])
         headers = ['#','Tenant','VRF']
-        numbers_tenant_vrf = [str(len(tenant_vrf_list))] + tenant_vrf_list
+        #import pdb; pdb.set_trace()
+        numbers_tenant_vrf = [[str(len(tenant_vrf_list))] + x[1] for x in tenant_vrf_list] 
         sizes = get_column_sizes(numbers_tenant_vrf, minimum=5, baseminimum=headers)
-        columnsizes = {'column2':sizes[0],'column3':sizes[1]}
-        print('{0:{column1}} | {1:{column2}} | {2:{column3}}'.format(*headers,column1=len(str(len(tenant_vrf_list)))+2,**columnsizes))
-        print('{empty:-<{column1}}-|-{empty:-<{column2}}-|-{empty:-<{column3}}'.format(empty='',column1=len(str(len(tenant_vrf_list)))+2,**columnsizes))
-        for num,tenantVrf in enumerate(tenant_vrf_list,1):
-            print('{num:{column1}} | {tenant:{column2}} | {vrf:{column3}}'.format(num=str(num) + '.)',column1=len(str(len(tenant_vrf_list)))+2,tenant=tenantVrf[0],vrf=tenantVrf[1],**columnsizes))
+        columnsizes = {'column1':sizes[0]+2,'column2':sizes[1],'column3':sizes[2]}
+        print('{0:{column1}} | {1:{column2}} | {2:{column3}}'.format(*headers,**columnsizes))
+        print('{empty:-<{column1}}-|-{empty:-<{column2}}-|-{empty:-<{column3}}'.format(empty='',**columnsizes))
+        for num,tenantVrf in enumerate(numbers_tenant_vrf,1):
+            print('{num:{column1}} | {tenant:{column2}} | {vrf:{column3}}'.format(num=str(num) + '.)',tenant=tenantVrf[1],vrf=tenantVrf[2],**columnsizes))
         while True:
             chosenvrf = custom_raw_input("\nSelect tenant|vrf #: ")
             if chosenvrf.isdigit() and int(chosenvrf) > 0 and int(chosenvrf) <= len(tenant_vrf_list) + 1:
                 break
             else:
                 continue
-        tenantandvrf = tenant_vrf_list[int(chosenvrf)-1]
+        tenantandvrf = numbers_tenant_vrf[int(chosenvrf)-1]
+        selected_scope = tenant_vrf_list[int(chosenvrf)-1][0]
+        tenantandvrf = ':'.join(tenantandvrf[1:])
+        if tenantandvrf.endswith(':'):
+            tenantandvrf = tenantandvrf[:-1]
         while True:
             endpoint = custom_raw_input("What is the Endpoint IP address: ")
             if re.match('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', endpoint):
@@ -119,10 +143,72 @@ def main(import_apic,import_cookie,user):
                 print('\nInvalid entry, try again...\n')
                 continue
         additionaloptions = None
-        cmd='iping {endpoint} -t 1 -i 0 -V {vrf}'.format(endpoint=endpoint,vrf=':'.join(tenantandvrf))
+        cmd='iping {endpoint} -V {vrf}'.format(endpoint=endpoint,vrf=tenantandvrf)
         if options:
-            cmd = cmd + ' ' + additionaloptions
+            while True:
+                count = custom_raw_input("Number of pings <count> [5]: ") or '5'
+                count = count.strip().lstrip()
+                if count != "" and count.isdigit():
+                    cmd = cmd + ' -c ' + str(count) 
+                    break
+                else:
+                    print('\nInvalid entry, try again...\n')
+                    continue
+            while True:
+                packetsize = custom_raw_input("<packetsize> [1500]: ") or '1500'
+                packetsize = packetsize.strip().lstrip()
+                if packetsize != "" and packetsize.isdigit():
+                    cmd = cmd + ' -s ' + str(packetsize) 
+                    break
+                else:
+                    print('\nInvalid entry, try again...\n')
+                    continue
+            while True:
+                dfbit = custom_raw_input("<df-bit> [N]: ") or 'N'
+                dfbit = dfbit.strip().lstrip()
+                if dfbit != "" and dfbit[0].upper() == 'Y':
+                    cmd = cmd + ' -F' 
+                    break
+                elif dfbit != "" and dfbit[0].upper() == 'N':
+                    break
+                else:
+                    print('\nInvalid entry, try again...\n')
+                    continue
+            while True:
+                timeout = custom_raw_input("<timeout> [2]: ") or '2'
+                timeout = timeout.strip().lstrip()
+                if timeout != "" and timeout.isdigit():
+                    cmd = cmd + ' -t ' + str(timeout) 
+                    break
+                else:
+                    print('\nInvalid entry, try again...\n')
+                    continue
+            while True:
+                wait = custom_raw_input("<interval per packet sec> [0]: ") or '0'
+                wait = wait.strip().lstrip()
+                if wait != "" and wait.isdigit():
+                    cmd = cmd + ' -i ' + str(wait) 
+                    break
+                else:
+                    print('\nInvalid entry, try again...\n')
+                    continue
+            svilist = gather_svi(apic,cookie,scope=selected_scope,leaf=chosenleafs[0])
+            l2bd_dict = gather_l2BD(apic,cookie,leaf=chosenleafs[0])
+            for num,x in enumerate(svilist,1):
+                print('{}.) {:7} {}'.format(num,x, l2bd_dict[x.replace('vlan','')]['name']))
+            while True:
+                source = custom_raw_input("Source interface: ")
+                source = source.strip().lstrip()
+                if source == "":
+                    break
+                if source != "" and source.isdigit() and int(source) > 0 and int(source) <= len(svilist) + 1:
+                    cmd = cmd + ' -S ' + str(svilist[int(source)-1]) 
+                    break
+                else:
+                    print('\nInvalid entry, try again...\n')
+                    continue
             remotesshcommand(cmd,user=user,device=deviceip)
         else:
+            cmd += ' -t 1 -i 0' 
             remotesshcommand(cmd,user=user,device=deviceip)
         custom_raw_input('\n[End of Results]')
